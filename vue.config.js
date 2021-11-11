16aecda69fb45eca287a7cbc15b23f41a3ce567b
Dmitriy Blyndaruk <dmitriy.blyndaruk@gmail.com>
2021-11-11 15:23:28 +0200

    add tailwind/upd eslint

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,23 @@
-// module.exports = {
-//   css: {
-//     loaderOptions: {
-//       sass: {
-//         additionalData: `
-//           @import "@/styles/app.scss";
-//         `
-//       }
-//     }
-//   }
-// };
+const path = require('path');
+
+const vueSrc = './src';
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, vueSrc),
+      },
+      extensions: ['.js', '.vue', '.json'],
+    },
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "@/assets/styles/app.scss";
+        `,
+      },
+    },
+  },
+};
