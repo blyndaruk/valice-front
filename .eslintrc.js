8cd9b272b3073442c2ea55130b529168424d5131
Dmitriy Blyndaruk <dmitriy.blyndaruk@gmail.com>
2021-11-20 15:17:16 +0200

    rework with nuxt

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,72 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    browser: true,
+    node: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier'
   ],
-  // "plugins": ["@typescript-eslint"],
-  // parserOptions: {
-  //   "parser": {
-  //     "ts": "@typescript-eslint/parser",
-  //     "js": "espree",
-  //     "<template>": "espree"
-  //   },
-  // }
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    quotes: [2, 'single', { avoidEscape: true }],
-    '@typescript-eslint/no-var-requires': 0,
-    'class-methods-use-this': ['error', {
-      exceptMethods: [
-        'data',
-        // react lifecycle methods, from the airbnb rule
-        'render',
-        'getInitialState',
-        'getDefaultProps',
-        'getChildContext',
-        'componentWillMount',
-        'componentDidMount',
-        'componentWillReceiveProps',
-        'shouldComponentUpdate',
-        'componentWillUpdate',
-        'componentDidUpdate',
-        'componentWillUnmount',
-
-        // vue lifecycle methods
-        'beforeCreate',
-        'created',
-        'beforeMount',
-        'mounted',
-        'beforeUpdate',
-        'updated',
-        'activated',
-        'deactivated',
-        'beforeDestroy',
-        'destroyed',
-        'errorCaptured',
-      ],
-    }],
-    'lines-between-class-members': 'off',
-    'semi': 'warn',
-    // '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
+  plugins: [
   ],
-};
+  // add your custom rules here
+  rules: {}
+}
